f2f462b71eed1be95bc978d8f8b3d5f11836e52a
ldadal <dada_@abv.bg>
2022-05-10 23:40:43 +0300

    rename

--- py basic may 2022/4. first step exam/basketball_equipment.py
+++ py basic may 2022/4. first step exam/basketball_equipment.py
@@ -2,6 +2,6 @@ yearly_fee = int(input())
 shoes = yearly_fee - yearly_fee * 0.4
 clothes = shoes - shoes * 0.2
 ball = clothes * 0.25
-other = ball * 0.2
+other = ball * 0.20
 total = yearly_fee + shoes + clothes + ball + other
 print(total)
