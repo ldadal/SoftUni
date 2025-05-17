f65a0a80a174c4d11df1dace1ff0fd97e9485539
penko <dada_@abv.bg>
2025-05-17 17:51:50 +0300

    _

--- Python Basics May 2022/StartOverAgain/functions/01_smallest_of_three_numbers.py
+++ Python Basics May 2022/StartOverAgain/functions/01_smallest_of_three_numbers.py
@@ -1,9 +1,13 @@
-def smallest_num (num_one, num_two, num_three):
-    result = min(num_one,num_two,num_three)
-    return result
-
+def smallest_num (numbers):
+    # result = min(numbers)
+    # return result
+    min_num = float("Inf")
+    for num in numbers:
+        if min_num>num:
+            min_num=num
+    return min_num
 
 num1 = int(input())
 num2 = int(input())
 num3 = int(input())
-print(smallest_num(num1,num2,num3))
+print(smallest_num([num1,num2,num3]))
