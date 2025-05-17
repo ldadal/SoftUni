9dd4f7b6e5000913d5ffe25db42f65dad18db03a
penko <dada_@abv.bg>
2025-05-17 18:12:05 +0300

    _

--- Python Basics May 2022/StartOverAgain/functions/02_add_and_subtract.py
+++ Python Basics May 2022/StartOverAgain/functions/02_add_and_subtract.py
@@ -1,13 +1,15 @@
 def add_and_substract(num1, num2, num3):
-    def sum_numbers(first, second):
-        sum = first + second
-        return sum
-
-    def subtract(first, second, third):
-        subtracted = sum_numbers(first, second) - third
-        return subtracted
-
     return subtract(num1, num2, num3)
+
+
+def add(first, second):
+    sum = first + second
+    return sum
+
+
+def subtract(first, second, third):
+    subtracted = add(first, second) - third
+    return subtracted
 
 
 num_one = int(input())
