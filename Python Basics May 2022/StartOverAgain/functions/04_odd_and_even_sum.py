559c9194b2b576578ad886aad38c83a0d84a7a6b
penko <dada_@abv.bg>
2025-05-17 18:31:13 +0300

    _

--- Python Basics May 2022/StartOverAgain/functions/04_odd_and_even_sum.py
+++ Python Basics May 2022/StartOverAgain/functions/04_odd_and_even_sum.py
@@ -1,6 +1,7 @@
 def sum_of_even_and_odd(number_as_string):
     odd = 0
     even = 0
+    result = []
     number = int(number_as_string)
     for _ in range(len(number_as_string)):
         if (number % 10) % 2 == 0:
@@ -8,9 +9,12 @@ def sum_of_even_and_odd(number_as_string):
         else:
             odd += number % 10
         number = number // 10
-    print(f"Odd sum = {odd}, Even sum = {even}")
+    # print(f"Odd sum = {odd}, Even sum = {even}")
+    result.append(odd)
+    result.append(even)
+    return result
 
 
 number_str = input()
-sum_of_even_and_odd(number_str)
+print(f"Odd sum = {sum_of_even_and_odd(number_str)[0]}, Even sum = {sum_of_even_and_odd(number_str)[1]}")
 
