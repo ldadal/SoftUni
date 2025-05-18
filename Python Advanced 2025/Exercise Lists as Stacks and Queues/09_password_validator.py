63c415a36ae3832373012deb0869142021317b30
penko <dada_@abv.bg>
2025-05-18 11:41:18 +0300

    _

--- Python Advanced 2025/Exercise Lists as Stacks and Queues/09_password_validator.py
+++ Python Advanced 2025/Exercise Lists as Stacks and Queues/09_password_validator.py
@@ -1,21 +1,49 @@
-def pass_checker(password):
-    count = 0
-    symbol_checker = 0
+# def pass_checker(password):
+#     count = 0
+#     symbol_checker = 0
+#     for sym in password:
+#         s = ord(sym)
+#         if sym.isdigit():
+#             count += 1
+#         if sym.isalnum():
+#             symbol_checker += 1
+#     if 6 > len(password) or len(password) > 10:
+#         print("Password must be between 6 and 10 characters")
+#     if symbol_checker < len(password):
+#         print("Password must consist only of letters and digits")
+#     if count < 2:
+#         print("Password must have at least 2 digits")
+#     if len(password) >= 6 and len(password) <= 10 and symbol_checker == len(password) and count >= 2:
+#         print("Password is valid")
+
+
+def lenght_cheker(password):
+    if 6 <= len(password) <= 10:
+        return True
+
+
+def sym_checker(password):
+    sym_count = 0
     for sym in password:
-        s = ord(sym)
-        if sym.isdigit():
-            count += 1
         if sym.isalnum():
-            symbol_checker += 1
-    if 6 > len(password) or len(password) > 10:
-        print("Password must be between 6 and 10 characters")
-    if symbol_checker < len(password):
-        print("Password must consist only of letters and digits")
-    if count < 2:
-        print("Password must have at least 2 digits")
-    if len(password) >= 6 and len(password) <= 10 and symbol_checker == len(password) and count >= 2:
-        print("Password is valid")
+            sym_count += 1
+    return sym_count
+
+
+def digit_checker(password):
+    count_digit = 0
+    for n in password:
+        if n.isdigit():
+            count_digit += 1
+    return count_digit
 
 
 password = input()
-pass_checker(password)
+if not lenght_cheker(password):
+    print("Password must be between 6 and 10 characters")
+if sym_checker(password) < len(password):
+    print("Password must consist only of letters and digits")
+if digit_checker(password) < 2:
+    print("Password must have at least 2 digits")
+if lenght_cheker(password) and sym_checker(password) == len(password) and digit_checker(password) >= 2:
+    print("Password is valid")
--- Python Basics May 2022/StartOverAgain/functions/05_even_numbers.py
+++ Python Basics May 2022/StartOverAgain/functions/05_even_numbers.py
@@ -1,10 +1,13 @@
 def even_only(num):
     return num % 2 == 0
 
-numbers_as_string = input().split()
-numbers = []
-for num in numbers_as_string:
-    numbers.append(int(num))
 
+# numbers_as_string = input().split()
+# numbers = []
+# for num in numbers_as_string:
+#     numbers.append(int(num))
+numbers = [int(x) for x in input().split()] # same as lines from 5 to 8
 # for x in filter(even_only, numbers): even.append(x)
 print(list(filter(even_only, numbers)))
+
+# print(list(filter(lambda x: x % 2 == 0, numbers)))
